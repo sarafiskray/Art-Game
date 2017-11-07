ebcda865355af78e9c19eac1f8faa2e2f00cf924
Joshenberg <jfrothen@uvm.edu>
2017-11-07 18:03:59 -0500

    supp

---
+++ NormalBrush.h
@@ -0,0 +1,23 @@
+//
+// Created by Joshenberg
+//
+
+/*
+ * Goals for this file:
+ * A brush that is implemented normally. Has a single color, size
+ * Its xPos and yPos are both relative to the character's
+ */
+
+#ifndef THE_STATISTICS_NORMALBRUSH_H
+#define THE_STATISTICS_NORMALBRUSH_H
+
+class NormalBrush : public Brush {
+private:
+    Color color;
+    
+public:
+
+
+};
+
+#endif //THE_STATISTICS_NORMALBRUSH_H
