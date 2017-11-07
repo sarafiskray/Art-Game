83f4590ced8abc84b0bcd4f90ccd965aa3d47b36
Andrew John Ridgely <andrew.j.ridgely@uvm.edu>
2017-11-07 18:04:00 -0500

    Started declaration of character header file, adding constructors

--- Character.h
+++ Character.h
@@ -5,7 +5,33 @@
 #ifndef THE_STATISTICS_CHARACTER_H
 #define THE_STATISTICS_CHARACTER_H
 
+struct skin {
+    double red;
+    double green;
+    double blue;
+};
 
+struct point {
+    int x;
+    int y;
+};
+
+class Character {
+private:
+    skin fill;
+    int speed;
+public:
+    point location;
+
+    //constructors
+    Character();
+    Character(location p);
+    Character(skin f);
+    Character(int speed);
+    Character(skin f, location p);
+
+
+};
 
 
 #endif //THE_STATISTICS_CHARACTER_H
---
+++ Splatter.h
@@ -0,0 +1,8 @@
+//
+// Created by Andrew Ridgely on 11/7/17.
+//
+
+#ifndef THE_STATISTICS_SPLATTER_H
+#define THE_STATISTICS_SPLATTER_H
+
+#endif //THE_STATISTICS_SPLATTER_H
