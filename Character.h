0e6d0f0da1afaf8f50375ef2d3bd06ab43b8d6f3
Andrew John Ridgely <andrew.j.ridgely@uvm.edu>
2017-11-17 16:43:41 -0500

    edited character h file with all methods and constructors

--- Character.h
+++ Character.h
@@ -5,29 +5,22 @@
 #ifndef THE_STATISTICS_CHARACTER_H
 #define THE_STATISTICS_CHARACTER_H
 
+
+//Josh and Saraf the character will be a rectangle to make it
+//easier for collosion detection. Maybe we can draw a character
+//inside rectangle to have it look like a person
 struct skin {
     double red;
     double green;
     double blue;
 };
 
-struct Point {
+struct point {
     int x;
     int y;
 };
 
 class Character {
-private:
-    skin fill;
-    int speed;
-    Brush currentBrush;
-    vector<Brush> brushes;
-    Point location;
-
-    //Two Momentums for controlling how the character responds and keeps moving after the player is not still running
-    int horizontalMomentum;
-    int verticalMomentum;
-
 public:
     point location;
 
@@ -37,20 +30,14 @@ public:
         DottedBrush db = brushes[1];
         currentBrush = brushes[0];
     };
+
     Character(location p);
-    Character(skin f);
-    Character(int speed);
-    Character(Brush brush);
     Character(skin f, location p, Brush brush);
+    Character(skin f, int xIn, int yIn);
 
+    //Destructor
+    ~Character();
 
-    void changeBrush() {
-           for (int i = 0; i < brushes.size(); i++) {
-               if (brushes[i] == currentBrush) {
-                   currentBrush = brushes[i+1];
-               }
-           }
-    }
 
     //getters
 
@@ -60,18 +47,41 @@ public:
     int getX() const;
     //Returns y of location
     int getY() const;
-
+    //gets size of character
+    double getArea() const;
+    //gets color of character
+    color getSkin() const;
 
     //setters
+    void setSkin(skin f);
+    void setSkin(double r, double g, double b);
+    void setLocation(location p);
+    void setLocation(int xIn, int yIn);
+
 
 
     //Methods
     //Controls the character if the player is not pressing anything; lets momentum do most of the work. Also calls getBeneath()
     void be();
+    void changeBrush();
+    //character moves left and right on x axis
+    void moveLeft(int deltaX);
+    void moveRight(int deltaX);
+    //character changes position on y axis
+    void jump(int deltaY);
+    
 
-    void moveLeft();
-    void moveRight();
-    void jump();
+private:
+    skin fill;
+    int speed;
+    double width;
+    double height;
+    Brush currentBrush;
+    vector<Brush> brushes;
+
+    //Two Momentums for controlling how the character responds and keeps moving after the player is not still running
+    int horizontalMomentum;
+    int verticalMomentum;
 
 
 };
