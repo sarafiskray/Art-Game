215cbf82722ef36df28377a66607edb9ae3d68c2
Joshenberg <jfrothen@uvm.edu>
2017-11-18 23:56:43 -0500

    Added fill to splatter so that the file i/o would work

--- Splatter.h
+++ Splatter.h
@@ -1,8 +1,28 @@
 //
 // Created by Andrew Ridgely on 11/7/17.
+// Will be much further implemented with Graphics
 //
+
+struct skin {
+    double red;
+    double green;
+    double blue;
+};
 
 #ifndef THE_STATISTICS_SPLATTER_H
 #define THE_STATISTICS_SPLATTER_H
+
+class Splatter {
+
+private:
+    skin fill;
+
+public:
+    Splatter(int r, int g, int b) {
+        fill.red = r;
+        fill.green = g;
+        fill.blue = b;
+    }
+};
 
 #endif //THE_STATISTICS_SPLATTER_H
