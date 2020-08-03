91b542d411236eee0b7ae70780ec0941a29abfed
Saraf Ray <sarafiskray@gmail.com>
2020-08-03 16:24:13 -0400

    cleaned up build

---
+++ cmake-build-debug/CMakeFiles/main.cpp.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/saraf/Documents/Art-Game/Brush.cpp" "/Users/saraf/Documents/Art-Game/cmake-build-debug/CMakeFiles/main.cpp.dir/Brush.cpp.o"
+  "/Users/saraf/Documents/Art-Game/Character.cpp" "/Users/saraf/Documents/Art-Game/cmake-build-debug/CMakeFiles/main.cpp.dir/Character.cpp.o"
+  "/Users/saraf/Documents/Art-Game/DottedBrush.cpp" "/Users/saraf/Documents/Art-Game/cmake-build-debug/CMakeFiles/main.cpp.dir/DottedBrush.cpp.o"
+  "/Users/saraf/Documents/Art-Game/Eraser.cpp" "/Users/saraf/Documents/Art-Game/cmake-build-debug/CMakeFiles/main.cpp.dir/Eraser.cpp.o"
+  "/Users/saraf/Documents/Art-Game/Headings.cpp" "/Users/saraf/Documents/Art-Game/cmake-build-debug/CMakeFiles/main.cpp.dir/Headings.cpp.o"
+  "/Users/saraf/Documents/Art-Game/Map.cpp" "/Users/saraf/Documents/Art-Game/cmake-build-debug/CMakeFiles/main.cpp.dir/Map.cpp.o"
+  "/Users/saraf/Documents/Art-Game/Shapes.cpp" "/Users/saraf/Documents/Art-Game/cmake-build-debug/CMakeFiles/main.cpp.dir/Shapes.cpp.o"
+  "/Users/saraf/Documents/Art-Game/Splatter.cpp" "/Users/saraf/Documents/Art-Game/cmake-build-debug/CMakeFiles/main.cpp.dir/Splatter.cpp.o"
+  "/Users/saraf/Documents/Art-Game/graphics.cpp" "/Users/saraf/Documents/Art-Game/cmake-build-debug/CMakeFiles/main.cpp.dir/graphics.cpp.o"
+  "/Users/saraf/Documents/Art-Game/main.cpp" "/Users/saraf/Documents/Art-Game/cmake-build-debug/CMakeFiles/main.cpp.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/main.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.cpp.dir/Brush.cpp.o"
+  "CMakeFiles/main.cpp.dir/Character.cpp.o"
+  "CMakeFiles/main.cpp.dir/DottedBrush.cpp.o"
+  "CMakeFiles/main.cpp.dir/Eraser.cpp.o"
+  "CMakeFiles/main.cpp.dir/Headings.cpp.o"
+  "CMakeFiles/main.cpp.dir/Map.cpp.o"
+  "CMakeFiles/main.cpp.dir/Shapes.cpp.o"
+  "CMakeFiles/main.cpp.dir/Splatter.cpp.o"
+  "CMakeFiles/main.cpp.dir/graphics.cpp.o"
+  "CMakeFiles/main.cpp.dir/main.cpp.o"
+  "../bin/main.cpp.pdb"
+  "../bin/main.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
